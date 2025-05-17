70bf6086d53714e562029c44e12515a8b241b282
cygday <amethystpun98@gmail.com>
2025-05-17 12:41:29 +0545

    Update app.test.js

--- test/app.test.js
+++ test/app.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('../app');
 
 describe('GET /', () => {
-    it("should return 'hello, there'", async() => {
+    it("should return 'hello, there'", async () => {
       const res = await
       request(app).get("/");
       expect(res.text).toBe("hello, there"); 
