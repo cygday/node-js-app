9b962c88bf93391cebf475359a0c2f40d85ef987
cygday <amethystpun98@gmail.com>
2025-05-17 11:15:42 +0545

    updated files

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
-app.get('/', (req, res) =>
-res.send('hello, there!'));
+app.get('/', (req, res) => {
+res.send('hello, there!');
+});
 
 app.listen(port, () => console.log('app running on port ${port}'));
 
--- test/app.test.js
+++ test/app.test.js
@@ -2,9 +2,9 @@ const request = require('supertest');
 const app = require('../app');
 
 describe('GET /', () => {
-	it('should return hello, there', async () => {
-		const res = await
-		require(app).get('/');
-		expect(res.text).toBe('hello, there');
+	it("should return 'hello, there'", async () => {
+	const res = await
+request(app).get('/');
+	expect(res.text).toBe('hello, there');
 	});
 });
